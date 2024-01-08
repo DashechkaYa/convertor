084d15b81861e2ed4c7c0dedfcbeb38472c75ac2
Daria Pashchenko <kyivdaria@gmail.com>
2024-01-08 15:51:24 +0200

    finish work with teacher check

--- script/main.js
+++ script/main.js
@@ -24,10 +24,12 @@ return Number(num).toFixed(2);
 };
 
 const isNumber = function isNumber(num) {
-return !Number.isNaN(Number(num));                        // нащо нам перевірка на число якщо у нас інпут тип = число; і поясніть ще раз будь ласка цю перевірку (нащо нам 2 рази писати Number - саме в першому випадку нащо)
+return !Number.isNaN(Number(num));
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
+  const rateData = await getUSDUARate();  
+
   const $arrowsSwitch = document.getElementById('arrows-switch');
   const $form = document.getElementById('form');
   const $inputUSD = $form.usd;
@@ -42,42 +44,30 @@ document.addEventListener('DOMContentLoaded', async () => {
    if (isNumber(uah)) $inputUAH.value = round(uah);
   };
 
-  const rateData = await getUSDUARate();                       // чи має значення якби це записано було перед ф setInputValues
-
-  // $inputUSD.value = 1;
-  // $inputUAH.value = round(1 * rateData);
-  const USDinUAH = round($inputUSD.value / $inputUAH.value);
-
   setInputValues({usd: 1, uah: 1 * rateData});
 
-  $textUSD.innerHTML = `1 USD = ${$inputUAH.value} UAH`;          // можливо це винести кудись треба, наприклад у функцію
+  const USDinUAH = round($inputUSD.value / $inputUAH.value);
+
+  $textUSD.innerHTML = `1 USD = ${$inputUAH.value} UAH`;
   $textUAH.innerHTML = `1 UAH = ${USDinUAH} USD`;
 
   $inputUSD.addEventListener('input', (e) => {
     const {value} = e.target;
     setInputValues({uah: value * rateData})
-    // $inputUAH.value = round(value * rateData);                // навіщо ми це замінили на строку вище - навіщо взагалі обєкт передавати в ф?
   });
 
   $inputUAH.addEventListener('input', (e) => {
     const {value} = e.target;
     setInputValues({usd: value / rateData})
-    // $inputUSD.value = round(value / rateData);
   });
 
   $arrowsSwitch.addEventListener('click', () => {
-    // [$inputUAH.value, $inputUSD.value] = [$inputUSD.value, $inputUAH.value];
-    // $inputUAH.value = round($inputUAH.value * rateData);
-    // $inputUSD.value = round($inputUSD.value * rateData);                     тут деструктуризація походу не до місця 
-    
     setInputValues({
       usd: $inputUAH.value,
       uah: $inputUAH.value * rateData,
     })
-    // $inputUSD.value = $inputUAH.value;
-    // $inputUAH.value = round($inputUAH.value * rateData);
 
-    if($form.firstElementChild === $USDBlock) {                    // зробила переміщення блоків місцями; як інакше міняти місцями флажки і підписи з мовою
+    if($form.firstElementChild === $USDBlock) {
       $form.prepend($UAHBlock);
       $form.append($USDBlock);
     } else {
